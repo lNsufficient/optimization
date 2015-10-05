8275936d659abb575ac41103ca6ca83d25f465bd
Linnea <tfy12lst@student.lu.se>
2015-10-05 12:48:08 +0200

    Adding inexact (previously exakt) linesearch to Newton class

--- optimization.py
+++ optimization.py
@@ -58,9 +58,85 @@ class Newton(Optimization): #This should probably inherit from QuasiNewton inste
         #since the classes that will be created themself contain the hessian
         #method (even though this does not contain it)
 
-    def inexactLineSearch()
-    
-    def exactLineSearch()
+    def _inexactLineSearch_(x_k,s_k,f_bar,alphai=1):
+        '''
+        Calculate the alpha which minimizes the function supplied to the class in init
+
+        Args:
+            x_k: A vector containing the x values for which alpha is to be calculated.
+            s_k: A vector containing the direction to look in.
+            f_bar: The tolerance level, meaning all alpha that return a function value lower or equal to f_bar are ok.
+            alphai: The starting value of alpha, with default value 1, used to calculate the correct alpha.
+        Returns:
+            The alpha which minimizes the function supplied to the class
+        '''
+        self.fderive = self._derive_(x_k,s_k) #Self????
+        SomethingLarge=100
+        alpha_prev = 0
+        f_prev = self.function(x_k)
+        f0=self.function(x_k)
+        self.fderive0 = self.fderive(0) #Self???
+        mu=(f_bar-f0)/(self.rho*self.fderive0) 
+        for i in range(0,SomethingLarge):
+            f=self.function(x_k+alphai*s_k)
+            if f<=f_bar:
+                return alphai
+            if (f>f0+alphai*self.rho*self.fderive0) or f>=f_prev: #could be wrong, fletcher...f'(0)
+                ai=alpha_prev
+                bi=alphai
+                #terminate block end
+                return self._NextIteration_(ai,bi,i,x_k,s_k,f0)
+            f_deriv = self.fderive(alphai)
+            if abs(f_deriv)<=-self.sigma*self.fderive0:
+                return alphai
+            if f_deriv>=0:
+                ai = alphai
+                bi = alpha_prev
+                #terminate block end
+                return self._NextIteration_(ai,bi,i,x_k,s_k,f0)
+            if mu<=2*alphai-alpha_prev:
+                (alphai,alpha_prev) = (mu,alphai)
+            else:
+                tau1=9
+                (alphai,alpha_prev) = (self._choose_(2*alphai-alpha_prev,min(mu,alphai+tau1*(alphai-alpha_prev)),alphai)
+            f_prev=f
+            
+    def _NextIteration_(aj,bj,i,x_k,s_k,f0):   
+        somethinglarge=100
+        tau2=0.1
+        tau3=1/2
+        epsilon=10**(-10)
+        for j in range(i, somethinglarge):
+            alphaj=self._choose_(aj+tau2*(bj-aj),bj-tau3*(bj-aj))
+            f=self.function(x_k+alphaj*s_k)
+            fderivaj=self.fderive(aj)
+            if (aj-alphaj)*fderivaj<=epsilon):
+                return alphaj
+            if f>f0+self.rho*alphaj*self.fderive0 or f>=self.function(x_k+aj*s_k):
+                bj=alphaj
+            else:
+                f_deriv = self.fderive(alphaj) 
+                if abs(f_deriv)<=-self.sigma*self.fderive0:
+                    return alphaj
+                if (bj-aj)*f_deriv>=0:
+                    bj=aj
+                aj=alphaj
+        return alphaj
+
+    def _choose_(minaj,maxaj):
+        return (maxaj+minaj)/2
+
+    def _derive_(x,s): 
+        ''' 
+        Returns the derivative of (self.)function with respect to alpha
+        '''
+        h=10**(-10)
+        snorm =N.linalg.norm(s)
+        return lambda alpha: (self.function(x+(alpha+h)*s)-self.function(x+(alpha-h)*s))/(2*h*snorm)
+        
+
+    def _exactLineSearch_()
+        
 
 class GoodBroyden(Newton):
     #Rank 1 update -- see wikipedia broyden's method
